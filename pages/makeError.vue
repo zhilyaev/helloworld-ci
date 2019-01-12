debdf7c764c9ccbaedc01d8986ad3734261b3695
zhilyaev <zhilyaev.dmitriy@gmail.com>
2019-01-12 19:43:30 +0300

    send sentry error

---
+++ pages/makeError.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Send Sentry Error</h1>
+    <button @click="$sentry.captureException(new Error('Test Error for Sentry'))">
+      Send Sentry Error
+    </button>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "makeError"
+  }
+</script>
+
+<style scoped>
+
+</style>
